55f3d5680f56f9825766a587eddfbbb9ab84e01d
Wyrdn <33638673+Wyrdn@users.noreply.github.com>
2018-04-04 21:13:12 -0400

    Update Task3Test5.c

--- test/Task3Test5.c
+++ test/Task3Test5.c
@@ -13,10 +13,7 @@ int main(){
 	//test a non-root user process
 	procID = exec(proc*, argNum, paraArr**);
 
-	int *stat = this.exitStatus.get(procID); //pointer to procs exit status
-	printf("The exit status is %d\n", stat*) //check exit status
-
-	result = join(procID, stat*);
+	result = join(procID, 2);
 	if (result = 1){
 		printf("proc1 exited successfully with a good exit status.");
 	} else if (result = 0) {
@@ -27,6 +24,6 @@ int main(){
 	
 	//Assuming this is the root, halt the machine via exit().
 	printf("The machine should halt here.\n");
-	this.exit(0);
+	exit(0);
 	
 }
