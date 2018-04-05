5bcf3648647b2562345fe117d300ac71633db075
Wyrdn <33638673+Wyrdn@users.noreply.github.com>
2018-04-04 21:11:32 -0400

    Update Task3Test3.c

--- test/Task3Test3.c
+++ test/Task3Test3.c
@@ -11,21 +11,18 @@ int main(){
 	char **paraArr = arr; //Array with valid parameters.
 	int procID, result;
 
-	userProcess foreigner = new userProcess(); //Not sure if this would work.
-
 	//ProcessIDs, obtained via exec().
 	int valChildID = exec(proc*, argNum, paraArr**);
-	int foreignID = foreigner.exec(proc*, argNum, paraArr**);
+	int foreignID = exec(proc*, argNum, paraArr**); // gonna need to do this elswhere.
 	int nullID = 12004;
 
 	//exitStatuses, obtained via exit().
-	int* valChildPtr = exitStatus.get(valChildID);
+	//int* valChildPtr = exitStatus.get(valChildID);
 	int* invalChildPtr = 3;
-	int* foreignPtr = foreigner.exitStatus.get(foreignID);
-	int* nullPtr = exitStatus.get(nullID);
+	//int* foreignPtr = foreigner.exitStatus.get(foreignID);
 
 	//test join with non-existent child.
-	result = join(nullID, nullPtr);
+	result = join(nullID, 0);
 	If (result = -1){
 		printf("Return value for joining a non existing process is correct"); 
 	} else {
@@ -33,8 +30,8 @@ int main(){
 	}
 
 	//test join with same process twice.
-	join(valChildID, valChildPtr);
-	result = join(valChildID, valChildPtr);
+	join(valChildID, 2);
+	result = join(valChildID, 2);
 	If (result = -1){
 		printf("Return value for joining the same process is correct"); 
 	} else {
@@ -42,20 +39,11 @@ int main(){
 	}
 	
 	//test join with a foreign process(join with a child of another process).
-	result = join(foreignID, foreignPtr);
+	result = join(foreignID, 2);
 	If (result = -1){
 		printf("Return value for joining with a foreign child process is correct"); 
 	} else {
 		printf("Return value for joining with a foreign child process is incorrect");
-	}
-	
-	foreigner.exit(1);
-	//test join with a foreign process(join with another parent).
-	result = join(foreigner.processID, foreigner.exitStatus.get(foreigner.processID));
-	If (result = -1){
-		printf("Return value for joining another parent process is correct"); 
-	} else {
-		printf("Return value for joining another parent process is incorrect");
 	}
 
 	//test join with a child possessing bad exit status 
